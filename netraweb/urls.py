08b9390c6687f4c9f1fd58deb40dfdfe49ddc829
Umang Zala <34640672+umangzala@users.noreply.github.com>
2020-07-27 15:30:31 +0530

    new

--- netraweb/urls.py
+++ netraweb/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from . import views
+from netra import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path("index",views.index,name="index"),
-    path('',views.change_det,name='change_det'),
+    path('change_det',views.change_det,name='change_det'),
     path('change_detection',views.change_detection,name='change_detection'),
     path('change_detection_result',views.change_detection_result,name='change_detection_result'),
     path('feature_extraction',views.feature_extraction,name='feature_extraction'),
     path('fea_ext',views.fea_ext,name='fea_ext'),
     path('login',views.login,name='login'),
     path('loader',views.loader,name='loader'),
-    path('new_login',views.new_login,name='new_login'),
+    path('',views.new_login,name='new_login'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
